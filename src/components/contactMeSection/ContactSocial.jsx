716295b77be20767cfa58c264a91ad879353888c
Hp2612 <harireact404@gmail.com>
2025-06-18 21:01:55 +0530

    linkfix

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -6,7 +6,7 @@ const ContactSocial = () => {
   return (
    <div className="flex gap-4 ">
     <SingleContactSocial link="#" Icon={() => <FiArrowRight color="orange" />} />
-      <SingleContactSocial link="https://www.linkedin.com/in/hari-prasad-722841235/"      Icon={FaLinkedinIn} />
+      <SingleContactSocial link="https://www.linkedin.com/in/hari-prasad-722841235?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"      Icon={FaLinkedinIn} />
       <SingleContactSocial link="https://github.com/Hp2612"                                Icon={FiGithub} />
      
        </div>
